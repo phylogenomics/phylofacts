c0270c06fa73fdc79fab76ab9f9be81d89568f3d
Nicholas Letourneau <nickl@Paon.local>
2011-06-24 14:48:54 -0700

    pf4.0 first import of an empty rails3.0.9 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phylogenomics::Application.config.secret_token = '2dbef050120e61cae801a1557764c4e047c888adb93fbcc07fea5528b92602229c8cf5a3bfa296679a53f2675e4b8d73e8dfc96d468b3e10041cfa3ab4a49fbb'
