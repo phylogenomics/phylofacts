881f3ec537d847bf23c7177d2c8c1e31312fc8c5
Shailen Tuli <shailen.tuli@gmail.com>
2011-07-07 15:07:30 -0700

    Required accession string and wrote rspec tests.

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -1,4 +1,6 @@
 class Accession < ActiveRecord::Base
   has_many :protein_accessions
   has_many :proteins, :through => :protein_accessions
+
+  validates :accession, :presence => true
 end
--- spec/models/accession_spec.rb
+++ spec/models/accession_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Accession do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new accession" do 
+    accession = Accession.new(:accession => "P12345")
+    accession.save
+    Accession.first.accession.should == "P12345"
+  end
+
+  it "should require the presence of the accession string" do
+    accession = Accession.new
+    lambda {accession.save!}.should raise_error(ActiveRecord::RecordInvalid)
+  end
 end
