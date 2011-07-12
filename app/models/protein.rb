cc77a82e99e283bb3a6f80a119b3ee4ef2fb9483
Nicholas Letourneau <nick@pathridden.com>
2011-07-12 16:15:46 -0700

    first stage of loading uniprot data

--- app/models/protein.rb
+++ app/models/protein.rb
@@ -4,4 +4,33 @@ class Protein < ActiveRecord::Base
   has_many :accession_links
   has_many :accessions, :through => :accession_links
 
+
+  def append_accessions(accessions, type)
+    self.accessions << accessions.each.inject([]) do |array, ac_string|
+      array << type.new(:accession => ac_string)
+    end
+  end
+
+  def self.create_from_uniprot(uniprot_string)
+    uniprot_data = Bio::SPTR201107.new(uniprot_string)
+    
+    prot = Protein.new
+    prot.sequence = uniprot_data.seq
+    
+    link = AccessionLink.new
+    link.primary = true
+    link.accession = UniprotAccession.new(:accession => uniprot_data.accession)
+
+    prot.accession_links << link
+
+    prot.save
+
+    non_primary_accessions = uniprot_data.accessions - [uniprot_data.accession]
+    unless non_primary_accessions.empty?
+      prot.append_accessions(non_primary_accessions, UniprotAccession)
+      prot.save
+    end
+
+    return prot
+  end
 end
--- spec/models/protein_spec.rb
+++ spec/models/protein_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Protein do
+  describe '#create' do
+    it 'should create a protein from a uniprot data string' do
+      uniprot_string = File.read("#{Rails.root}/spec/O14727.txt")
+      prot = Protein.create_from_uniprot(uniprot_string)
+
+      prot.accessions.first.accession.should == "O14727"
+      prot.accessions.length.should == 20
+    end
+  end
+
   describe '#test model linkage' do
-    it 'should create a protein with one primary uniprot accession' do
+    it 'should create a protein with one primary accession' do
       accession = UniprotAccession.new
       accession.accession = "uniprot_stuff"
 
@@ -13,27 +23,30 @@ describe Protein do
       link.primary = true
       link.accession = accession
 
+      prot.accession_links << link
+
       prot.save.should == true
+
+      new_prot = Protein.find_by_sequence("AAA")
+
+      new_prot.accessions.first.accession.should == accession.accession
+      new_prot.accession_links.first.primary == true
     end
 
-    it 'should create a protein with many uniprot accessions' do
+    it 'should create a protein with many secondary accessions' do
       accession_strings = ["1", "2", "3", "4", "5"]
 
       prot = Protein.new
       prot.sequence = "AAH"
       
-      u_accessions = accession_strings.inject([]) do |array, ac_string|
-        array << UniprotAccession.new(:accession => ac_string)
-        array
-      end
-
-      prot.accessions = u_accessions
+      prot.append_accessions(accession_strings, UniprotAccession)
       prot.save.should == true
 
       new_prot = Protein.find_by_sequence("AAH")
       prot.accessions.first.accession.should == accession_strings.first
     end
   end
+
   it { should have_many(:accessions) }
   it { should have_many(:accession_links) }
 end
