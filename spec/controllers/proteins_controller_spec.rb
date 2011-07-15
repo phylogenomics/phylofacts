ddf8da587d7c51711702e14bfbcec523868e354f
Nicholas Letourneau <nick@pathridden.com>
2011-07-15 15:45:41 -0700

    modified (mostly deleted) tests for new protein schema

--- spec/controllers/proteins_controller_spec.rb
+++ spec/controllers/proteins_controller_spec.rb
@@ -2,17 +2,24 @@ require 'spec_helper'
 
 describe ProteinsController do
   describe "GET index" do
-    it "should list all accessions" do 
+    it "should list all proteins" do 
       get :index
       response.should be_success
     end
   end
 
+  describe "GET search" do
+    it "should redirect to proteins_url on blank query string" do
+      get :search, 'q' => ""
+
+      response.should redirect_to proteins_path
+    end
+  end
+
   describe "GET show" do
     it "should obtain the record for the accession" do 
-      #get :show, :id => "garbage"
-      #response.should be_success
-      pending "create all the stubs to make this test work"
+      get :show, :id => "A0B2E5"
+      response.should be_success
     end
   end
 end
